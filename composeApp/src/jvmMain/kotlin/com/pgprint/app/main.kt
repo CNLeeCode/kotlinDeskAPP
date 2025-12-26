2e629bce8a596ae7c98bf8ddcaa52d0adea211e7
Lee <5231597+lezphp@user.noreply.gitee.com>
2025-12-26 20:53:47 +0800

    windows

---
+++ composeApp/src/jvmMain/kotlin/com/pgprint/app/main.kt
@@ -0,0 +1,13 @@
+package com.pgprint.app
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Pgprint",
+    ) {
+        App()
+    }
+}
